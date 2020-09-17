f343fc7ec7583831c5daa3487317832cf1510101
zhaojunkai1007 <zhaojunkai19921007@foxmail.com>
2020-09-17 11:56:20 +0800

    111

---
+++ src/pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button type="primary" @click="dianji">按钮</button>
+    <div class="zhaojunkai">11111</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+    dianji () {
+      wx.showToast({
+        title: '成功',
+        icon: 'success',
+        duration: 2000
+      })
+    }
+  },
+
+  created () {
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .zhaojunkai {
+    color: #362c96;
+  }
+</style>
